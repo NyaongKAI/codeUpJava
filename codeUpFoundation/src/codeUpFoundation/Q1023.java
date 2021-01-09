2a872e6510d4599840e97ca7784fc6c945b66783
NyaongKAI <wx1997@naver.com>
2021-01-09 21:49:08 +0900

    codeUp Foundation 100
    Question 1023
    
    Java ver.

---
+++ codeUpFoundation/src/codeUpFoundation/Q1023.java
@@ -0,0 +1,14 @@
+package codeUpFoundation;
+
+import java.util.Scanner;
+
+public class Q1023 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String[] A = a.split("\\.");
+		System.out.printf("%s\n%s", A[0], A[1]);
+	}
+}
